b61b359cf3e1840fa8c3a5337ca1051f27853634
Jay-Hin <jayhinyip@gmail.com>
2019-12-13 10:59:57 +1100

    Update Databasestart.py

--- Databasestart.py
+++ Databasestart.py
@@ -6,6 +6,7 @@ from evenness import evenness
 from makeNetwork import makeNetwork
 from highCostIdentifier import highCostIdentifier
 from hiPoIdentifier import Responsiveness
+import csv
 
 data = pd.read_excel('SNA_DATA.xlsx')
 
@@ -16,7 +17,10 @@ uniqueRecipientList = [n for n in recipientlist if n not in senderlist]
 ID_list = senderlist + uniqueRecipientList
 
 with open('employee_file.csv', mode='w') as employee_file:
+    fieldnames = ['ID_list']
+    writer.writeheader()
     employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    for i in range len(ID_list)
     employee_writer.writerow([ID_list])
 
 
