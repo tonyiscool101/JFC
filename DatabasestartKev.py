6d2ba195785f35e43126e0e21ad1db7972eeba35
Jay-Hin <jayhinyip@gmail.com>
2019-12-13 16:58:34 +1100

    DID IT

--- DatabasestartKev.py
+++ DatabasestartKev.py
@@ -10,27 +10,26 @@ df = df.drop_duplicates('Sender', keep='first')
 df2 = xls.parse(xls.sheet_names[0])
 df2 = df2.drop_duplicates('Recipient', keep='first')
 
-senderlist = df['Sender'].tolist()
-officelist = df['SendersOffice'].tolist()
+
 senderboys= []
-recipientlist = df2['Recipient'].tolist()
-recipientoffice = df2['RecipientsOffice'].tolist()
 recipientboth = []
 
 senderboys.append(df['Sender'].tolist())
 senderboys.append(df['SendersOffice'].tolist())
+senderboys.append(df['SendersJobTitle'].tolist())
 recipientboth.append(df2['Recipient'].tolist())
 recipientboth.append(df2['RecipientsOffice'].tolist())
+recipientboth.append(df2['RecipientsJobTitle'].tolist())
 
 uniqueRecipientList = [n for n in zip(*recipientboth) if n not in zip(*senderboys)]
 uniqueRecipientList2 = list(map(list, zip(*uniqueRecipientList)))
 
 ID_list = np.hstack((senderboys,uniqueRecipientList2))
-
+print(ID_list)
 bigList = []
 bigList.append(ID_list) #Add senderlist to big list
 
-with open("midman2.csv", mode="w", newline='') as f:
+with open("Employee_database.csv", mode="w", newline='') as f:
         writer = csv.writer(f)
         writer.writerows(zip(*ID_list))
 
