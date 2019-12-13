b9e6cd30516c538099ec26422cf1fe8d2154be04
Jay-Hin <jayhinyip@gmail.com>
2019-12-13 16:34:56 +1100

    DID IT

--- DatabasestartKev.py
+++ DatabasestartKev.py
@@ -8,9 +8,7 @@ xls = pd.ExcelFile('SNA_DATA.xlsx')
 df = xls.parse(xls.sheet_names[0])
 df = df.drop_duplicates('Sender', keep='first')
 df2 = xls.parse(xls.sheet_names[0])
-df2 = df.drop_duplicates('Recipient', keep='first')
-
-
+df2 = df2.drop_duplicates('Recipient', keep='first')
 
 senderlist = df['Sender'].tolist()
 officelist = df['SendersOffice'].tolist()
@@ -19,25 +17,21 @@ recipientlist = df2['Recipient'].tolist()
 recipientoffice = df2['RecipientsOffice'].tolist()
 recipientboth = []
 
-
 senderboys.append(df['Sender'].tolist())
 senderboys.append(df['SendersOffice'].tolist())
 recipientboth.append(df2['Recipient'].tolist())
 recipientboth.append(df2['RecipientsOffice'].tolist())
 
-uniqueRecipientList = [n for n in recipientboth if n not in senderboys]
-ID_list = senderboys + uniqueRecipientList
-print(np.shape(uniqueRecipientList))
-print(np.shape(senderboys))
-print(np.shape(ID_list))
+uniqueRecipientList = [n for n in zip(*recipientboth) if n not in zip(*senderboys)]
+uniqueRecipientList2 = list(map(list, zip(*uniqueRecipientList)))
 
+ID_list = np.hstack((senderboys,uniqueRecipientList2))
 
 bigList = []
-
 bigList.append(ID_list) #Add senderlist to big list
-
-
 
 with open("midman2.csv", mode="w", newline='') as f:
         writer = csv.writer(f)
         writer.writerows(zip(*ID_list))
+
+
