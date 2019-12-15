96480d2fc30550a79eef0fa10229c4523c864b5d
Jay-Hin <jayhinyip@gmail.com>
2019-12-16 09:04:19 +1100

    Added comments

--- DatabasestartKev.py
+++ DatabasestartKev.py
@@ -4,13 +4,14 @@ import pandas as pd
 import csv
 import numpy as np
 
+# Read data & separate into senders and recipient lists
 xls = pd.ExcelFile('SNA_DATA.xlsx')
 df = xls.parse(xls.sheet_names[0])
 df = df.drop_duplicates('Sender', keep='first')
 df2 = xls.parse(xls.sheet_names[0])
 df2 = df2.drop_duplicates('Recipient', keep='first')
 
-
+# Create sender and recipent and its respective office lists
 senderboys= []
 recipientboth = []
 
@@ -21,14 +22,17 @@ recipientboth.append(df2['Recipient'].tolist())
 recipientboth.append(df2['RecipientsOffice'].tolist())
 recipientboth.append(df2['RecipientsJobTitle'].tolist())
 
+# Drop any duplicates in recipient list
 uniqueRecipientList = [n for n in zip(*recipientboth) if n not in zip(*senderboys)]
 uniqueRecipientList2 = list(map(list, zip(*uniqueRecipientList)))
 
+# Transpose ID list from vector to a list in 2 columns
 ID_list = np.hstack((senderboys,uniqueRecipientList2))
 print(ID_list)
 bigList = []
 bigList.append(ID_list) #Add senderlist to big list
 
+# Open a separate new csv file as the database of employees
 with open("Employee_database.csv", mode="w", newline='') as f:
         writer = csv.writer(f)
         writer.writerows(zip(*ID_list))
